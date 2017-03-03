3cd9262c1d5eb9e4c6bd0db8abc5c2bc43f7a09c
makovich <makovich@great.pm>
2017-03-03 14:56:58 +0300

    Reorganize file structure related to Docker Hub

--- build-image.sh
+++ build-image.sh
@@ -6,8 +6,8 @@ repository="local"
 docker rmi $repository/musl-mono-microcontainer-build || true
 docker rmi $repository/musl-mono-microcontainer || true
 
-docker build --rm -t=$repository/musl-mono-microcontainer-build -f docker/Dockerfile.build .
-docker run --rm $repository/musl-mono-microcontainer-build cat runtime.tar > docker/runtime.tar
+docker build --rm -t=$repository/musl-mono-microcontainer-build -f Dockerfile.build .
+docker run --rm $repository/musl-mono-microcontainer-build cat runtime.tar > artifacts/runtime.tar
 
-docker build --rm -t=$repository/musl-mono-microcontainer -f docker/Dockerfile.runtime docker
+docker build --rm -t=$repository/musl-mono-microcontainer .
 docker rmi $repository/musl-mono-microcontainer-build
